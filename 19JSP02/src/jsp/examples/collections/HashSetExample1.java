c161d9afaa1dfcfe4b69e629c80b9a372ef50ee3
philiptruongacc2 <139836689+philiptruongacc2@users.noreply.github.com>
2025-01-13 07:09:59 +0700

    Update HashSetExample1.java
    f21

--- 19JSP02/src/jsp/examples/collections/HashSetExample1.java
+++ 19JSP02/src/jsp/examples/collections/HashSetExample1.java
@@ -16,7 +16,7 @@ public class HashSetExample1 {
         // Creating hashset
         Set<String> set = new HashSet<String>();
         // Add objects to hashset
-        set.add("Java221111112");
+        set.add("Javagf2");
         set.add("C++");
         set.add("PHP");
         set.add("Java11");
