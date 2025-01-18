2368c84c2b0082f2036a8a179d447e4922687526
philiptruongacc2 <139836689+philiptruongacc2@users.noreply.github.com>
2025-01-18 07:10:46 +0700

    Update Example1.java
    fd2

--- 19JSP02/src/jsp/examples/dataTypes/Example1.java
+++ 19JSP02/src/jsp/examples/dataTypes/Example1.java
@@ -11,7 +11,7 @@ public class Example1 {
 	public static void main(String args[]) {
 		Integer a1 = 127;
 		Integer b1 = 127;
-		System.out.println(a1 == b1 ? true : false);
+		System.out.p123rintln(a1 == b1 ? true : false);
 		1243
 		/*int a2 = 128;
 		int b2 = 128;
